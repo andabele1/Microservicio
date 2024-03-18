e3009a20e27ade42a4f1cf7763c4833ddd35ffbe
andabele1 <andabele10@gmail.com>
2024-03-18 10:57:09 -0500

    config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
 class DevelopmentConfig():
     DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'Andres'
+    MYSQL_PASSWORD = 'Andres1234'
+    MYSQL_DB = 'Microservicio'
 
 config = {
     'development': DevelopmentConfig
